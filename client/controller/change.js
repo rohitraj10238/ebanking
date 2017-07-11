364577fe8d49bfa0ea54ec877621fae3511193a2
banking-app/day <rohitraj10238@gmail.com>
2017-07-12 00:27:02 +0530

    file created

---
+++ client/controller/change.js
@@ -0,0 +1,22 @@
+	app.controller('change',function($scope,$state,$http)
+	{	
+	$scope.change=function()
+{
+	$http
+	({
+		method:'GET',
+		url:'/login',
+		params:
+		{
+			email : $scope.email,
+			password : $scope.password,
+			setpassword : $scope.password
+		}
+	})
+.then(function(response)
+	{
+		console.log(">>>>>>. response data",response.data);
+		$state.go('login');
+	})
+}
+});
