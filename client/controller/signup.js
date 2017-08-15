45efc8d2cdce799af1983e21d21cba23ac15b5bd
banking-app/day <rohitraj10238@gmail.com>
2017-08-15 18:11:40 +0530

    file edit

--- client/controller/signup.js
+++ client/controller/signup.js
@@ -1,25 +1,36 @@
 app.controller('signup',function($scope,$state,$http)
 {
+	
 $scope.signup=function()
 {
+    var x; 
 	$http
 	({
 		method:'POST',
 		url:'/signup',
-		params:
+		params: 
 		{
 			username : $scope.uname,
 			password : $scope.password,
 			address	:$scope.address,
-			email:$scope.email,
+			email:$scope.address,
 			phone:$scope.phoneno,
-			pan:$scope.pan
+			pan:$scope.pan,
+			aadhar:$scope.aadhar,
+			AccountBala:1000,
+			Bankacc:myFunction(x)
+
 		}
 	})
 	.then(function(response)
 	{
-     console.log("data sent in ",response);
+     console.log("data sent in ",response.data);
      $state.go('login');
 	})
 }
 })
+	function myFunction(x) 
+		{
+			
+    		 return(Math.floor((Math.random() * 1000000) + 1))
+		}
